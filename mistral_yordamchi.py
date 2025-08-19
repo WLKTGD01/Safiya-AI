9016282d5c08f7e48102bf96ae2601eb8d48c687
WLKTGD01 <brunerjack221@gmail.com>
2025-08-19 15:54:36 +0500

    Update mistral_yordamchi.py

--- mistral_yordamchi.py
+++ mistral_yordamchi.py
@@ -1,21 +1,17 @@
+# Synchronous Example
 from mistralai import Mistral
 
 def mistral_kpt(user):
-    api_key='fXEf24hfVLvmIEHRvilwOKN6hPpSET7b'
-    model = 'mistral-large-latest'
-    client = Mistral(api_key=api_key)
-    responce = client.chat.complete(
-        model=model,
-        messages=[
-                {"role": "system", 
-                "content": "Your name is Yordamchi AI, you were created by Normurodov Jasur Ibragimovich"
-                },
-                {"role": "user", 
-                "content": user
-                }
-            ]
-        )
-    return responce.choices[0].message.content
+    with Mistral(
+        api_key='fXEf24hfVLvmIEHRvilwOKN6hPpSET7b',
+    ) as mistral:
 
+        res = mistral.chat.complete(model="mistral-small-latest", messages=[
+            {
+                "content": user,
+                "role": "user",
+            },
+        ], stream=False)
 
-# print(mistral_kpt('Salom ai'))
+    # Handle response
+        return res
